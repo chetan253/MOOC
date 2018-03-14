a99350d304de11d167266eb90e572574dcf24767
chetan253 <cpatil0@outlook.com>
2018-03-13 23:13:09 -0400

    JS demo 1

---
+++ udemy-The-Web-Developer-Bootcamp/Excercise-Section08/JSstalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+alert("Check javascript console now");
+console.log("Your full name is, "+ firstName+" "+ lastName);
+console.log("You are "+ age + " years old.");
---
+++ udemy-The-Web-Developer-Bootcamp/Excercise-Section08/agecalc.js
@@ -0,0 +1,3 @@
+var age = prompt("What is your age?");
+var days = age*365;
+alert("You're roughly "+ days +" days old");
